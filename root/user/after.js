5a1422227e0fdb6c97cd60dc8830d8aab468329e
vuf <hashed@outlook.jp>
2016-11-12 22:48:58 +0900

    ユーザー用スクリプトの初期状態の書き方を少しだけ変更

--- root/user/after.js
+++ root/user/after.js
@@ -3,7 +3,7 @@
 // ! Main Process Side Script
 // This script will be invoked after the Discord App is loaded.
 
-const info = require('../invoker/info.js').acquire();
-
 
 // Your code goes here...
+
+const info = require('../invoker/info.js').acquire();
--- root/user/before.js
+++ root/user/before.js
@@ -3,7 +3,7 @@
 // ! Main Process Side Script
 // This script will be invoked before the Discord App is loaded.
 
-const info = require('../invoker/info.js').acquire();
-
 
 // Your code goes here...
+
+const info = require('../invoker/info.js').acquire();
