2b2785a5562dd871337d6bdec0ef8b9477dd916e
vuf <hashed@outlook.jp>
2016-11-12 23:34:20 +0900

    example 追加

---
+++ example/mainpage.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(() => {
+    const types = {
+        'ogg': 'audio',
+    };
+
+
+    function ensureArray(value) {
+        return Array.isArray(value) ? value : [value];
+    }
+
+    (function monitorAttachments() {
+        let attachments = document.querySelectorAll('.attachment:not(.processed-attachment)');
+        attachments.forEach(attachment => {
+            const fileUrl = attachment.getElementsByTagName('a')[0].href;
+            if (!fileUrl) return;
+
+            const extension = fileUrl.replace(/^.+\./, '');
+            const mediaTypes = types[extension] ? ensureArray(types[extension]) : ['video', 'audio'];
+
+            for (let mediaType of mediaTypes) {
+                let media = document.createElement(mediaType);
+
+                const mimeType = `${mediaType}/${extension}`;
+                if (!media.canPlayType(mimeType)) {
+                    continue;
+                }
+
+                media.className = 'attachment-media';
+
+                media.src = fileUrl;
+                media.controls = true;
+                media.preload = 'metadata';
+
+                attachment.appendChild(media);
+
+                break;
+            }
+
+            attachment.classList.add('processed-attachment');
+        });
+
+        setTimeout(monitorAttachments, 1000);
+    })();
+
+})();
