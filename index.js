0e85ff7502af3eb53dbaa5c0a025c388a050b624
vuf <hashed@outlook.jp>
2016-11-19 05:26:13 +0900

    Refactor: index.js

--- index.js
+++ index.js
@@ -1,259 +1,56 @@
-#!/usr/bin/env node
 'use strict';
 
-const childProcess = require('child_process');
-const fs = require('fs-extra');
-const path = require('path');
+const Discord = require('./lib/discord.js');
+const DiscordLoader = require('./lib/discord-loader.js');
 
-const asar = require('asar-lite');
 const minimist = require('minimist');
-
-const isRelease = !process.mainModule;
-const resources = isRelease && require('nexeres.js');
-
-
-/**
- * Check if the path to the Discord App is valid.
- * @param appDir {string} - '/path/to/Discord/'
- * @returns {boolean} - true if valid
- */
-function isValidAppDir(appDir) {
-    if (!fs.existsSync(path.join(appDir, 'app.ico'))) return false;
-    if (!fs.existsSync(path.join(appDir, 'packages'))) return false;
-    return true;
-}
-
-/**
- * Find the Discord App directory
- * @returns {string|null} - '/path/to/Discord/' if found, null if not
- */
-function findAppDir() {
-    let candidates = [];
-    if (isRelease) {
-        candidates.push(__dirname);
-    }
-    switch (process.platform) {
-        case 'win32':
-            if (process.env.LOCALAPPDATA) {
-                candidates.push(path.join(process.env.LOCALAPPDATA, 'Discord'));
-            }
-            break;
-    }
-
-    candidates = candidates.filter(isValidAppDir);
-    return candidates.length ? candidates[0] : null;
-}
 
 
 const argv = minimist(process.argv.slice(2), {
     default: {
-        appdir: findAppDir(),
+        appdir: null,
         profile: null,
         debug: false,
     },
 });
+
 const {appdir, profile, debug} = argv;
 
-if (debug) {
-    console.info('debug mode enabled.');
-    console.info('\nargv:');
-    console.dir(argv);
-    console.info('\nresources:');
-    console.dir(resources && resources.keys());
-    /*
-    // currently this will cause an error silently
-    console.info('\nprocess:');
-    console.dir(process);
-    //*/
-    console.info('\n');
-}
 
-if (!appdir) {
+const discordDir = appdir || Discord.searchDiscordDir()[0];
+
+if (!discordDir) {
     console.log('Usage: discord-loader --appdir /path/to/Discord/ [--profile profilename] [--debug]');
     process.exit(1);
 }
 
-if (profile && /^\.+$|\.$|[\/\\]/.test(profile)) {
+
+if (profile && !DiscordLoader.isValidProfile(profile, false)) {
     console.log('Invalid profilename specified');
     process.exit(1);
 }
 
-console.info('AppDir:', appdir);
 
-
-const rootLoaderDir = path.join(appdir, 'loader');
-const invokerDir = path.join(rootLoaderDir, 'invoker');
-const userDir = path.join(rootLoaderDir, 'user');
-const profilesDir = path.join(rootLoaderDir, 'profiles');
-const tempDir = path.join(rootLoaderDir, 'temp');
-
-
-/**
- * Writes data to file synchronously in JSON format.
- * @param file {string} - filename
- * @param data {Object} - data
- */
-function writeJsonSync(file, data) {
-    return fs.writeFileSync(file, JSON.stringify(data, null, '  '));
+if (debug) {
+    console.info('Debug mode enabled.');
+    console.info('\n');
 }
 
-/**
- * Copy resource files (./resources/*).
- * @param src  {string} - path to resource directory copy files from
- * @param dest {string} - path to directory copy files to
- */
-function extractResourceDirSync(src, dest) {
-    if (isRelease) {
-        const nSrc = path.normalize(src) + path.sep;
-        const files = resources.keys().filter(file => file.substr(0, nSrc.length) === nSrc);
-        files.forEach(file => {
-            const content = resources.get(file).toString();
-            fs.writeFileSync(path.join(dest, path.relative(src, file)), content);
-        });
-    } else {
-        fs.copySync(path.join(__dirname, 'resources', src), dest, {
-            clobber: true,
-        });
-    }
-}
-
-/**
- * Read resource files (./resources/*).
- * @param src {string} - path to resource file
- * @returns {string} - content of the file (utf-8)
- */
-function readResourceSync(src) {
-    if (isRelease) {
-        return resources.get(path.normalize(src)).toString();
-    } else {
-        return fs.readFileSync(path.join(__dirname, 'resources', src), 'utf-8');
-    }
-}
+console.info('DiscordDir:', discordDir);
 
 
-/**
- * Prepare files requried to launch discord-loader.
- * This is called only when this is first launch of discord-loader.
- */
-function initialize() {
-    if (fs.existsSync(rootLoaderDir)) return;
+const discordLoader = new DiscordLoader(discordDir);
 
-    extractResourceDirSync('root', rootLoaderDir);
-
-    fs.ensureDirSync(profilesDir);
-    fs.ensureDirSync(tempDir);
-}
-
-/**
- * Update core files.
- * This will be called every launch of discord-loader.
- */
-function update() {
-    extractResourceDirSync('root/invoker', invokerDir);
-}
-
-/**
- * Extract and modify package.json of the Discord App and make Discord App boot discord-loader.
- * This will be called every launch of discord-loader (to follow updates of the Discord App).
- * @param progDir {string} - '/path/to/Discord/app-#.#.#/'
- */
-function modify(progDir) {
-    const resDir = path.join(progDir, 'resources');
-    const resAppDir = path.join(resDir, 'app');
-    const resAppAsar = path.join(resDir, 'app.asar');
-    const appPackageJson = path.join(resAppDir, 'package.json');
-    const appLoaderJs = path.join(resAppDir, 'loader.js');
-
-    if (!fs.existsSync(resAppAsar)) return;
-
-    let pkg = JSON.parse(asar.extractFile(resAppAsar, 'package.json'));
-    pkg.main = path.relative(path.dirname(appPackageJson), appLoaderJs).replace(/\\/g, '/');
-    fs.ensureDirSync(resAppDir);
-    writeJsonSync(appPackageJson, pkg);
-
-    const pathToRootInvoker = path.relative(path.dirname(appLoaderJs), invokerDir).replace(/\\/g, '/') + '/';
-    const pathToPackageJson = path.relative(path.dirname(appLoaderJs), appPackageJson).replace(/\\/g, '/');
-
-    let loaderScript = readResourceSync('loader-template.js');
-    loaderScript = loaderScript.replace(/["']?<INVOKER>["']?/g, JSON.stringify(pathToRootInvoker));
-    loaderScript = loaderScript.replace(/["']?<PACKAGE_JSON>["']?/g, JSON.stringify(pathToPackageJson));
-    fs.writeFileSync(appLoaderJs, loaderScript);
-}
+discordLoader.installOrUpdate(true);
 
 
-/**
- * Launch discord-loader (through modified Discord App).
- * @param appDir {string} - '/path/to/Discord/'
- * @param profile {string|null} - profile id
- */
-function launchDiscord(appDir, profile) {
-    const loaderConfig = {
-        profile,
-        debug,
-        appDir,
-        invokerDir,
-        profilesDir,
-        tempDir,
-        userDir,
-    };
+const proc = discordLoader.launch(profile, debug);
 
-    let prog, args;
-    switch (process.platform) {
-        case 'win32':
-            if (!debug) {
-                prog = 'Update.exe';
-                args = ['-processStart', 'Discord.exe'];
-            } else {
-                const rxSemver = /^app-(\d+)\.(\d+)\.(\d+)/;
-                const apps = fs.readdirSync(appDir).filter(v => rxSemver.test(v)).sort((a, b) => {
-                    const matches = [a, b].map(v => v.match(rxSemver));
-                    for (let i = 1; i <= 3; i++) {
-                        const diff = parseInt(matches[1][i]) - parseInt(matches[0][i]);
-                        if (diff) return diff;
-                    }
-                    return 0;
-                });
-
-                if (!apps.length) {
-                    throw new Error(`No app found in ${appDir}.`);
-                }
-
-                const latestApp = apps[0];
-                console.log('Latest app:', latestApp);
-
-                prog = path.join(latestApp, 'Discord.exe');
-                args = [];
-            }
-            break;
-
-        default:
-            throw new Error(`Unsupported platform: ${process.platform}`);
-    }
-
-    console.log();
-
-    prog = path.join(appDir, prog);
-    const proc = childProcess.spawn(prog, args, {
-        env: {
-            __loaderConfig: JSON.stringify(loaderConfig),
-        },
-    });
-    ['stdout', 'stderr'].forEach(intf => {
-        proc[intf].pipe(process[intf]);
-    });
-    proc.on('exit', code => {
-        console.log(`Child process exited with code ${code}`);
-        process.exit(code);
-    });
-}
-
-
-initialize();
-update();
-
-const apps = fs.readdirSync(appdir).filter(dir => /^app-/.test(dir));
-apps.forEach(dir => {
-    modify(path.join(appdir, dir));
+['stdout', 'stderr'].forEach(intf => {
+    proc[intf].pipe(process[intf]);
 });
 
-launchDiscord(appdir, profile);
+proc.on('exit', code => {
+    console.log(`Child process exited with code ${code}`);
+    process.exit(code);
+});
