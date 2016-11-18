ea7bc895e23ac98451f9ddbd19275a10aeb63a08
vuf <hashed@outlook.jp>
2016-11-19 05:56:52 +0900

    Example: Fix error when uploaded files, remove closure

--- example/mainpage.js
+++ example/mainpage.js
@@ -1,48 +1,47 @@
 /* eslint-env browser */
 'use strict';
 
-(() => {
-    const types = {
-        'ogg': 'audio',
-    };
+const types = {
+    'ogg': 'audio',
+};
 
+function ensureArray(value) {
+    return Array.isArray(value) ? value : [value];
+}
 
-    function ensureArray(value) {
-        return Array.isArray(value) ? value : [value];
-    }
+(function monitorAttachments() {
+    let attachments = document.querySelectorAll('.attachment:not(.processed-attachment)');
+    attachments.forEach(attachment => {
+        const fileLink = attachment.getElementsByTagName('a')[0];
+        if (!fileLink) return;
 
-    (function monitorAttachments() {
-        let attachments = document.querySelectorAll('.attachment:not(.processed-attachment)');
-        attachments.forEach(attachment => {
-            const fileUrl = attachment.getElementsByTagName('a')[0].href;
-            if (!fileUrl) return;
+        const fileUrl = fileLink.href;
+        if (!fileUrl) return;
 
-            const extension = fileUrl.replace(/^.+\./, '');
-            const mediaTypes = types[extension] ? ensureArray(types[extension]) : ['video', 'audio'];
+        const extension = fileUrl.replace(/^.+\./, '');
+        const mediaTypes = types[extension] ? ensureArray(types[extension]) : ['video', 'audio'];
 
-            for (let mediaType of mediaTypes) {
-                let media = document.createElement(mediaType);
+        for (let mediaType of mediaTypes) {
+            let media = document.createElement(mediaType);
 
-                const mimeType = `${mediaType}/${extension}`;
-                if (!media.canPlayType(mimeType)) {
-                    continue;
-                }
-
-                media.className = 'attachment-media';
-
-                media.src = fileUrl;
-                media.controls = true;
-                media.preload = 'metadata';
-
-                attachment.parentElement.insertBefore(media, attachment.nextSibling);
-
-                break;
+            const mimeType = `${mediaType}/${extension}`;
+            if (!media.canPlayType(mimeType)) {
+                continue;
             }
 
-            attachment.classList.add('processed-attachment');
-        });
+            media.className = 'attachment-media';
 
-        setTimeout(monitorAttachments, 1000);
-    })();
+            media.src = fileUrl;
+            media.controls = true;
+            media.preload = 'metadata';
 
+            attachment.parentElement.insertBefore(media, attachment.nextSibling);
+
+            break;
+        }
+
+        attachment.classList.add('processed-attachment');
+    });
+
+    setTimeout(monitorAttachments, 1000);
 })();
