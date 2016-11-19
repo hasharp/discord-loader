13b58361390e853e66c0f235168167d94a0d1507
vuf <hashed@outlook.jp>
2016-11-19 12:09:57 +0900

    loaderconfig.js: .acquire() returns undefined instead of null, Add JSDoc comment

--- resources/root/invoker/loaderconfig.js
+++ resources/root/invoker/loaderconfig.js
@@ -6,14 +6,23 @@
 let loaderConfig;
 
 module.exports = {
+    /**
+     * Set loaderConfig.
+     * @param {loaderConfig} newLoaderConfig - loaderConfig
+     * @returns {boolean} - true if successful
+     */
     initialize(newLoaderConfig) {
         if (loaderConfig) return false;
         loaderConfig = newLoaderConfig;
         return true;
     },
 
+    /**
+     * Returns stored loaderConfig.
+     * @returns {loaderConfig|undefined} - loaderConfig if there is, undefined if not.
+     */
     acquire() {
-        if (!loaderConfig) return null;
+        if (!loaderConfig) return undefined;
         return Object.assign({}, loaderConfig);
     },
 };
